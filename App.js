b635e46bc5eb2a454c5362268dede78653131792
caricaDev <santos2tiago@gmail.com>
2019-03-15 15:31:11 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      // <Header headerText="Albums Bitch" />
+       <Header headerText={'Albums'} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
