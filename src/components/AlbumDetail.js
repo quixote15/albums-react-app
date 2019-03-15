f44b02ad056f8db5df1c20710667779acf5ea5c0
caricaDev <santos2tiago@gmail.com>
2019-03-15 17:39:43 -0300

    Creates reusable components

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import Card from './Card';
+import CardSection from './CardSection';
 
 
 const AlbumDetail = (props) =>{
    return (
-        <View>
+        <Card>
+            <CardSection>
             <Text>{props.album.title}</Text>
-        </View>
+            </CardSection>
+        </Card>
     )
 }
 
